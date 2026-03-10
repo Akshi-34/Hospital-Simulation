5357626fbdd23e01d5c6a31a9da5acd8774a2b9a
vaanithakkar <vthakkar491@myfcc.frederick.edu>
2026-03-09 20:40:31 -0400

    call bell class done

---
+++ src/CallBell.java
@@ -0,0 +1,9 @@
+public class CallBell extends Observations {
+    public boolean dangerous() {
+        return true;
+    }
+    public String data() {
+        return "Call bell pressed";
+    }
+    public CallBell() {}
+}
