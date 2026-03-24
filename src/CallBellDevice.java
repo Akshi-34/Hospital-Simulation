3d588b557abb16fd354dd192cb6b72c48fce0b9f
vaanithakkar <vthakkar491@myfcc.frederick.edu>
2026-03-23 20:36:21 -0400

    Call Bell Device Class added

---
+++ src/CallBellDevice.java
@@ -0,0 +1,15 @@
+public class CallBellDevice extends Devices {
+    private boolean active = false;
+    public Observations sample() {
+        if (!active && Simulation.random.nextDouble() < 0.01) {
+            active = true;
+        }
+        if (active) {
+            return new CallBellObservation();
+        }
+        return null;
+    }
+    public void reset() {
+        active = false;
+    }
+}
