028089f0e0207adeecc377551b3de616f3133251
vaanithakkar <vthakkar491@myfcc.frederick.edu>
2026-04-03 12:49:03 -0400

    added urgent alerts

--- src/Alert.java
+++ src/Alert.java
@@ -6,11 +6,20 @@ public class Alert {
         observation = obs;
         timeRaised = time;
         timeResolved = -1;
+        urgent = obs.dangerous();
     }
     public void resolve(int time){
         timeResolved = time;
     }
     public String toString(){
         return "Alert at time" + timeRaised + ": " + observation.data();
+    }
+    private boolean urgent;
+    public boolean isUrgent() {
+        return urgent;
+    }
+    public int getResolutionTime() {
+        if (timeResolved == -1) return -1;
+        return timeResolved - timeRaised;
     }
 }
